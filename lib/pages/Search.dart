0e17cf0d2ff5831ba57f241b05e385aba5d93e83
pktnnnnnn <pakintorn.tub@student.mahidol.edu>
2024-04-07 19:11:40 +0700

    add a Search page

---
+++ lib/pages/Search.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SearchPage());
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [searchbox()],
+      ),
+    );
+  }
+
+  Container searchbox() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Color(0xff1D1617).withOpacity(0.11),
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        )
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(15),
+          hintText: 'Search Pancake',
+          hintStyle: const TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+          prefixIcon: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          suffixIcon:
+              IconButton(onPressed: () {}, icon: Icon(Icons.filter_list)),
+        ),
+      ),
+    );
+  }
+}
