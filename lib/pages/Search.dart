cf81efdf21cdfdd478cccf5bffdd331c71744fcf
pktnnnnnn <pakintorn.tub@student.mahidol.edu>
2024-04-14 16:51:26 +0700

    add font theme and update a new search page

--- lib/pages/Search.dart
+++ lib/pages/Search.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: const SearchPage(),
-  ));
-}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -17,40 +13,127 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [searchbox()],
+      appBar: AppBar(
+        title: const Text('Groove'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SearchPage2()),
+              );
+            },
+            icon: const Icon(Icons.search_rounded),
+          )
+        ],
       ),
     );
   }
+}
 
-  Container searchbox() {
-    return Container(
-      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-      decoration: BoxDecoration(boxShadow: [
-        BoxShadow(
-          color: Color(0xff1D1617).withOpacity(0.11),
-          blurRadius: 40,
-          spreadRadius: 0.0,
-        )
-      ]),
-      child: TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(40),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: Colors.white,
-          contentPadding: const EdgeInsets.all(15),
-          hintText: 'Search ',
-          hintStyle: const TextStyle(color: Color(0xffDDDADA), fontSize: 14),
-          prefixIcon: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
+class SearchPage2 extends StatefulWidget {
+  const SearchPage2({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage2> createState() => _SearchPage2State();
+}
+
+class _SearchPage2State extends State<SearchPage2> {
+  String name = "";
+  List<Map<String, dynamic>> data = [
+    // Your data here
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  void addData() async {
+    for (var element in data) {
+      await FirebaseFirestore.instance.collection('users').add(element);
+    }
+    print('all data added');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+        title: Card(
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search_rounded),
+              hintText: 'Search',
+            ),
+            onChanged: (val) {
+              setState(() {
+                name = val;
+              });
+            },
           ),
         ),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshots) {
+          if (snapshots.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshots.data!.docs.length,
+            itemBuilder: (context, index) {
+              var data = snapshots.data!.docs[index].data() as Map<String, dynamic>;
+
+              if (name.isEmpty || data['name'].toString().toLowerCase().startsWith(name.toLowerCase())) {
+                return ListTile(
+                  title: Text(
+                    data['name'],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    data['email'],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(data['image']),
+                  ),
+                );
+              }
+              return Container();
+            },
+          );
+        },
+      ),
     );
   }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    home: SearchPage(),
+  ));
 }
